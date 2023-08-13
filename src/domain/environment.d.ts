57e48df02cb205939d4c2c78ca639de5542d6b31
Vitor Ricardo <vitorricardo@outlook.com>
2023-08-13 11:29:33 -0300

    chore: add config docker

--- src/domain/environment.d.ts
+++ src/domain/environment.d.ts
@@ -3,6 +3,12 @@ declare global {
     interface ProcessEnv {
       PORT: number;
       NODE_ENV: 'local' | 'production' | 'homolog';
+      MONGO_HOST: string;
+      MONGO_PORT: string;
+      MONGO_USER: string;
+      MONGO_PWD: string;
+      MONGO_DB: string;
+      MONGO_AUTH_SOURCE: string;
       REDIS_HOST: string;
       REDIS_PORT: number;
       REDIS_PASSWORD: string;
